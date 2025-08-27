5002ace703de525327ac53ce5cdeca4e1fe4d493
kaizhou <130264951+kaizhou24@users.noreply.github.com>
2025-08-26 22:17:50 -0400

    Load AlphaVantage stock data

--- hello_world.py
+++ hello_world.py
@@ -2,7 +2,7 @@ from pandas_datareader import data
 import matplotlib.pyplot as plt
 import pandas as pd
 import datetime as dt
-import urllib.request, json
+import requests, json
 import os
 import numpy as np
 import tensorflow as tf
@@ -10,5 +10,11 @@ from sklearn.preprocessing import MinMaxScaler
 
 API_KEY = 'KOSRKMA8X6FK2B86'
 TICKER = 'AAL'
-URL_STRING = None
-FILE_TO_SAVE = None
+URL = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={TICKER}&interval=5min&apikey={API_KEY}'
+
+r = requests.get(URL)
+data = r.json()
+df = pd.DataFrame(data)
+
+file_to_save = f'{TICKER}_data.csv'
+df.to_csv(file_to_save, index=False)
